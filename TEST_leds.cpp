6228ce49c94ed2326df7cac7429d7a9ac9a08764
Robin Simonot <rsimonot@magentinehealthcare.com>
2022-02-17 14:42:49 +0000

    LEDs not working / Temp (?) I'm convinced that I'm close

--- TEST_leds.cpp
+++ TEST_leds.cpp
@@ -2,64 +2,88 @@
 
 #define OEpin 50    // OE pin, all LEDs at the same time, is on GPIO17, pin#50
 
-int flashLEDs(int ADC_address)
+int flashLEDs(int fd)
 {
-    int fd = wiringPiI2CSetup(ADC_address);
     // Structure of the sent data : 3 bits for auto-increment (always '000' -> no increment)
     // Then 5 bits for the register : LED0 to LED5
-    int registers[6] = {00000010, 00000011, 00000100, 00000101, 00000110, 00000111};
+    int registers[6] = {0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
     int i = 0;
     for (int reg : registers) {
         std::cout << P << "Flashing LED #" << ++i << "  Reg value = " << reg << N << std::endl;
-        if (wiringPiI2CWrite(fd, reg) < 0 && wiringPiI2CWrite(fd, 11110000) < 0) {
+        if (wiringPiI2CWriteReg8(fd, reg, 0xff) < 0) {
             std::cout << R << "  [-] Problem for LED #" << i << N << std::endl;
             return -1;
         }
-        delay(800);
-        wiringPiI2CWrite(fd, 00000000),
-        //wiringPiI2CWriteReg8(fd, reg, 00000000);
+        digitalWrite(OEpin, LOW);
         delay(1500);
+        digitalWrite(OEpin, HIGH);
+
+        //wiringPiI2CWriteReg8(fd, reg, 0x00);
     }
 
     return 0;
 }
 
+int GRPPWM_leds(int fd, int GRPPWM_addr)
+{
+    std::cout << P << "Brightness MAX" << N << std::endl;
+    if (wiringPiI2CWriteReg8(fd, GRPPWM_addr, 0xff))    // 255/255
+        return -1;
+    digitalWrite(OEpin, LOW); delay(800);
+    digitalWrite(OEpin, HIGH);
+
+    std::cout << P << "Brightness MED" << N << std::endl;
+    if (wiringPiI2CWriteReg8(fd, GRPPWM_addr, 0x64))    // 100/255
+        return -2;
+    digitalWrite(OEpin, LOW); delay(800);
+    digitalWrite(OEpin, HIGH);
+    
+    std::cout << P << "Brightness OFF" << N << std::endl;
+    if (wiringPiI2CWriteReg8(fd, GRPPWM_addr, 0x00))    // 0/255
+        return -3;
+    digitalWrite(OEpin, LOW); delay(800);
+    digitalWrite(OEpin, HIGH);
+
+    return 0;
+}
+
+
 int main (void)
 {
-    //  First, use the OE pin to light up all the LEDs together one  time
-    //  Or turn them all down if they're already up
-    std::cout << P_B << "-> Enabling all the LEDs at the same time" << N << std::endl;
     wiringPiSetup();
-    pinMode(OEpin, INPUT);
-    digitalWrite(OEpin, LOW);
-    delay(1000);
-    std::cout << P << "> Done" << N << std::endl;
-
-    // Now controling all the LEDs at the same time
+    pinMode(OEpin, OUTPUT);
     int i2c_address_doc = 0x15;    // I2C Address of PCA9624PW 
+    int fd = wiringPiI2CSetup(i2c_address_doc);
     int ADC_address = detectADCDevice(i2c_address_doc);     // Just a confirmation for now, the function doesn't do that much job
     if (ADC_address < 0)
         return EXIT_FAILURE;
 
-    std::cout << P_B << "-> Flashing LEDs in" << P << " 3 ... " << std::endl;
-    delay(800);
-    std::cout << "... 2 ..." << std::endl;
-    delay(800);
-    std::cout << "... 1 ..." << N << std::endl;
-    delay(800);
-    if (flashLEDs(ADC_address) != 0) {
+    //  First, set up MODE1 and LEDOUT0/LEDOUT1
+    std::cout << P_B << "-> Initializing with the right parameters..." << N << std::endl;
+    wiringPiI2CWriteReg8(fd, 0x00, 0x00);
+    wiringPiI2CWriteReg8(fd, 0x0c, 0xff);
+    wiringPiI2CWriteReg8(fd, 0x0d, 0xff);
+    std::cout << P << "> Done" << N << std::endl;
+    
+    // Now controling the LEDs one by one
+    std::cout << P_B << "-> Flashing LEDs in" << P << " 3 ... " << std::endl; delay(800);
+    std::cout << "... 2 ..." << std::endl; delay(800);
+    std::cout << "... 1 ..." << N << std::endl; delay(800);
+    if (flashLEDs(fd) != 0) {
         std::cout << R_B << "ERR : Unable to flash LEDs" << N << std::endl;
         return EXIT_FAILURE;
     }
-    std::cout << G << "  [+]  All LEDs flashed !" << std::endl;
-
+    std::cout << G << "  [+]  All LEDs flashed !" << std::endl; delay(1000);
+    /*
     // Lastly, control all the LEDs' brightness at the same time as it will be done with the whole prototype
-
-    // Disable all the LEDs
-    std::cout << P_B << "-> Disabling all the LEDs" << N << std::endl;
-    digitalWrite(OEpin, HIGH);
-    std::cout << P << "> Done" << N << std::endl;
-
+    std::cout << P_B  << "-> Controling global brightness" << N << std::endl;
+    int GRPPWM_addr = 0x0a;
+    if (GRPPWM_leds(fd, GRPPWM_addr) != 0) {
+        std::cout << R_B << "ERR : Unable use GRPPWM" << N << std::endl;
+        return EXIT_FAILURE;
+    }
+    */
+    // Finish
     std::cout << P_B << "-> All jobs done, exiting." << N << std::endl;
 
     return EXIT_SUCCESS;
--- TEST_temperature.cpp
+++ TEST_temperature.cpp
@@ -8,20 +8,27 @@ int main (void)
     if (ADC_address < 0)
         return EXIT_FAILURE;
 
+    int id_register = 0x0f;
     int temp_register = 0x00;
     int fd = wiringPiI2CSetup(ADC_address);
+
+    wiringPiI2CWrite(fd, id_register);
+    int id = wiringPiI2CRead(fd);
+    if (id < 0) {
+        std::cout << R_B << "ERR : Unable to read from ADC Device" << N << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::cout << G << "  [+] Successfully read ID from ADC Device !" << N << std::endl;
+    std::cout << P_B << "ID value -> " << P << id << N << std::endl;
+
     wiringPiI2CWrite(fd, temp_register);
     int r_value = wiringPiI2CRead(fd);
     if (r_value < 0) {
         std::cout << R_B << "ERR : Unable to read from ADC Device" << N << std::endl;
         return EXIT_FAILURE;
     }
-    float voltage = (float)r_value / 255.0 * 3.3;    // calculate voltage    
-    float Rt = 10 * voltage / (3.3 - voltage);        //calculate resistance value of thermistor
-    float tempK = 1/(1/(273.15 + 25) + log(Rt/10)/3950.0); //calculate temperature (Kelvin)
-    float tempC = tempK -273.15;        //calculate temperature (Celsius)
-    std::cout << G << "  [+] Successfully read from ADC Device !" << N << std::endl;
-    std::cout << P_B << "TEMP value read -> " << P << r_value << N << std::endl;
+    std::cout << G << "  [+] Successfully read Temperature from ADC Device !" << N << std::endl;
+    std::cout << P_B << "TEMP value -> " << P << r_value << N << std::endl;
 
     return EXIT_SUCCESS;
 }
